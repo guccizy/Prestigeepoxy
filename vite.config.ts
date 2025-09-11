75bd4b818d2cf50fab72efdaf4a8dd0b68c5b459
WhyB <guccizy34@gmail.com>
2025-09-11 02:50:01 +0200

    Mail FIX + Logo reseau

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    rollupOptions: {
+      external: ['react-icons/si'],
+    },
+  },
   server: {
     host: true, // écoute sur toutes les IP
     port: 5173,
