de437d8bd02483cb65875fe08350169020edf712
WhyB <guccizy34@gmail.com>
2025-09-11 04:41:23 +0200

    Update texte bloc blog test fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import postcssImport from 'postcss-import';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,6 +10,13 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: ['react-quill'],
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        postcssImport()
+      ],
     },
   },
   server: {
