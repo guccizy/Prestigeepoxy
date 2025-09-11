0694d10cf2d2bd61d7eeeb68ceb3e343c69f4730
WhyB <guccizy34@gmail.com>
2025-09-11 04:42:34 +0200

    Update texte bloc blog test fix

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [
-        postcssImport()
+        postcssImport({ addModulesDirectories: ['node_modules'] })
       ],
     },
   },
